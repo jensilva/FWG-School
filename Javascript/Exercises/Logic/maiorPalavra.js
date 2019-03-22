bb44d214868d4f2603a11df4e96c8b8f17802e51
Jen <17807158+jensilva@users.noreply.github.com>
2019-03-22 19:16:15 +0000

    Update maiorPalavra.js

--- Javascript/Exercises/Logic/maiorPalavra.js
+++ Javascript/Exercises/Logic/maiorPalavra.js
@@ -1,7 +1,7 @@
 const maiorPalavra = frase => {
 	let maior = "";
 	let palavras = frase.split(" ");
-	for(i=0; i < palavras.length; i++){
+	for(let i=0; i < palavras.length; i++){
 		if(maior.length < palavras[i].length){
 			maior = palavras[i]
 		}
