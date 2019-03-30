11a9afc6a336e37beee0f61cc02c73006079089e
Jen <17807158+jensilva@users.noreply.github.com>
2019-03-30 00:25:07 +0000

    Create ex3.js

---
+++ Javascript/Exercises/basic/ex3.js
@@ -0,0 +1,29 @@
+<script>
+  /** Escreva um programa que reprsente uma grade de 8x8, usando quebras de linha. 
+A cada “#” deve ter um espaço em branco, como um tabuleiro de xadrez. 
+Utilizando o console.log deve ficar como isso: 
+
+# # # #
+ # # # # 
+# # # #
+ # # # # 
+# # # #
+ # # # # 
+# # # #
+ # # # #
+
+ */
+
+  for (let linha = 0; linha < 8; linha++) {
+    let quadrado = "";
+
+    for (let coluna = 0; coluna < 8; coluna++) {
+      if (coluna % 2 == 0) {
+        quadrado += " ";
+      } else {
+        quadrado += "#";
+      }
+    }
+    console.log(quadrado);
+  }
+</script>
